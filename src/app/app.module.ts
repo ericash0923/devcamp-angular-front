e7b9a37570ed073f02857ace1187e9cf1e28e946
ericash0923 <eric@gaglani.com>
2018-01-09 23:55:00 +0000

    implemented proposal interface with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,7 +17,9 @@ import { AppRoutingModule } from './app-routing.module';
 	declarations: [
 	  AppComponent,
 		HomepageComponent,
-		DocumentsComponent
+		DocumentsComponent,
+		ProposalListComponent,
+		ProposalNewComponent
 	],
 	bootstrap: [
 		AppComponent
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Document } from './documents';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
