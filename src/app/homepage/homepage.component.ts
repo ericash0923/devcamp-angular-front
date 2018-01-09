8d8c9d1ec82d129454f349c79607f3003acd2100
ericash0923 <eric@gaglani.com>
2018-01-09 00:30:58 +0000

    first commit

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'homepage',
+// 	templateUrl: './homepage.component.html',
+// 	styleUrls: ['./homepage.component.css']
+  template: '<h1>Home</h1>'
+})
+export class HomepageComponent {}
